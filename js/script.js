7be689100213b05af4feada82bc40c146b051b47
marco-gulino <marcogulino98@gmail.com>
2025-01-22 21:08:28 +0100

    [feat/html-js] added a button that deletes existing emails and adds new ones

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-//Scelgo quante email generare
-let userChoice = parseInt(prompt("Scegli quante email vuoi generare."));
 //Elementi OUTPUT del DOM
 const listEmail = document.getElementById("email-list");
 
+//Elementi INPUT del DOM
+const rerollEmail = document.querySelector(".btn");
+
+//Scelgo quante email generare
+let userChoice = parseInt(prompt("Scegli quante email vuoi generare."));
+
 //Faccio partire la richiesta AJAX verso l'API
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
 //Mando 10 richieste all'API
 for (let i = 0; i < userChoice; i++) {
   axios
@@ -23,3 +28,27 @@ for (let i = 0; i < userChoice; i++) {
       //Codice da esegure a prescindere dall'esito
     });
 }
+
+rerollEmail.addEventListener("click", () => {
+  //Svuoto la lista di Email
+  listEmail.innerHTML = "";
+  //Scelgo quante email generare
+  userChoice = parseInt(prompt("Scegli quante email vuoi generare."));
+  for (let j = 0; j < userChoice; j++) {
+    axios
+      .get(endpoint)
+      .then((responseObj) => {
+        //Codice da eseguire in caso di successo
+        const randEmail = responseObj.data.response;
+        //Contenuto della lista
+        listEmail.innerHTML += `<li class="list-group-item">${randEmail}</li>`;
+      })
+      .catch((error) => {
+        //Codice da eseguire in caso di errore
+        console.error(error);
+      })
+      .then(() => {
+        //Codice da esegure a prescindere dall'esito
+      });
+  }
+});
