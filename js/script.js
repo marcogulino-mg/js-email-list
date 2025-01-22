dd9c257592acba285d99febf02c42fe92361ce9a
marco-gulino <marcogulino98@gmail.com>
2025-01-22 18:42:22 +0100

    [feat/js]iterate i-times the number of responses from the API

--- js/script.js
+++ js/script.js
@@ -1,21 +1,25 @@
-const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
-
-//Faccio partire la richiesta AJAX verso l'API
-axios
-  .get(endpoint)
-  .then((responseObj) => {
-    //Codice da eseguire in caso di successo
-    const randEmail = responseObj.data.response;
-    //Contenuto della lista
-    listEmail.innerHTML += `<li>${randEmail}</li>`;
-  })
-  .catch((error) => {
-    //Codice da eseguire in caso di errore
-    console.error(error);
-  })
-  .then(() => {
-    //Codice da esegure a prescindere dall'esito
-  });
-
+//Scelgo quante email generare
+let userChoice = parseInt(prompt("Scegli quante email vuoi generare."));
 //Elementi OUTPUT del DOM
 const listEmail = document.getElementById("email-list");
+
+//Faccio partire la richiesta AJAX verso l'API
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+//Mando 10 richieste all'API
+for (let i = 0; i < userChoice; i++) {
+  axios
+    .get(endpoint)
+    .then((responseObj) => {
+      //Codice da eseguire in caso di successo
+      const randEmail = responseObj.data.response;
+      //Contenuto della lista
+      listEmail.innerHTML += `<li>${randEmail}</li>`;
+    })
+    .catch((error) => {
+      //Codice da eseguire in caso di errore
+      console.error(error);
+    })
+    .then(() => {
+      //Codice da esegure a prescindere dall'esito
+    });
+}
