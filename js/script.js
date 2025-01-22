841f0e6a70d7286afb1308e0587adf41a4f6e789
marco-gulino <marcogulino98@gmail.com>
2025-01-22 18:27:54 +0100

    [feat/js] requires API response with AJAX

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-
+//Faccio partire la richiesta AJAX verso l'API
+axios
+  .get(endpoint)
+  .then((responseObj) => {
+    //Codice da eseguire in caso di successo
+  })
+  .catch((error) => {
+    //Codice da eseguire in caso di errore
+    console.error(error);
+  })
+  .then(() => {
+    //Codice da esegure a prescindere dall'esito
+  });
