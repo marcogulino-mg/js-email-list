85e7508bc7ef6b070970556a0ae47ddca2bddbc0
marco-gulino <marcogulino98@gmail.com>
2025-01-23 10:48:45 +0100

    [feat/js] shortened the code by creating a function

--- js/script.js
+++ js/script.js
@@ -4,37 +4,17 @@ const listEmail = document.getElementById("email-list");
 //Elementi INPUT del DOM
 const rerollEmail = document.querySelector(".btn");
 
-//Scelgo quante email generare
-let userChoice = parseInt(prompt("Scegli quante email vuoi generare."));
-
 //Faccio partire la richiesta AJAX verso l'API
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-//Mando 10 richieste all'API
-for (let i = 0; i < userChoice; i++) {
-  axios
-    .get(endpoint)
-    .then((responseObj) => {
-      //Codice da eseguire in caso di successo
-      const randEmail = responseObj.data.response;
-      //Contenuto della lista
-      listEmail.innerHTML += `<li class="list-group-item">${randEmail}</li>`;
-    })
-    .catch((error) => {
-      //Codice da eseguire in caso di errore
-      console.error(error);
-    })
-    .then(() => {
-      //Codice da esegure a prescindere dall'esito
-    });
-}
-
-rerollEmail.addEventListener("click", () => {
+//Creo la funzione che andrà a svolgere la chiamata AJAX per richiamare le API Boolean
+//e che genererà tot email random quante scelte dall'utente
+function genEmail() {
   //Svuoto la lista di Email
   listEmail.innerHTML = "";
   //Scelgo quante email generare
-  userChoice = parseInt(prompt("Scegli quante email vuoi generare."));
-  for (let j = 0; j < userChoice; j++) {
+  let numEmail = parseInt(prompt("Scegli quante email vuoi generare."));
+  for (let j = 0; j < numEmail; j++) {
     axios
       .get(endpoint)
       .then((responseObj) => {
@@ -51,4 +31,9 @@ rerollEmail.addEventListener("click", () => {
         //Codice da esegure a prescindere dall'esito
       });
   }
-});
+}
+
+//Richiamo la funzione
+genEmail();
+//L'evento click attiva la funzione
+rerollEmail.addEventListener("click", genEmail);
